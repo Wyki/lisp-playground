99776cda2750e40123a5649a4b95654ba517df81
Wyki <morten.helwers@rwth-aachen.de>
2013-06-15 12:11:41 +0300

    Update html.lisp
    optimized

--- html.lisp
+++ html.lisp
@@ -1,74 +1,39 @@
-(defun replace-all (string part replacement &key (test #'char=))
-  "Returns a new string in which all the occurences of the part 
-is replaced with replacement."
-  (with-output-to-string (out)
-    (loop with part-length = (length part)
-          for old-pos = 0 then (+ pos part-length)
-          for pos = (search part string
-                            :start2 old-pos
-                            :test test)
-          do (write-string string out
-                           :start old-pos
-                           :end (or pos (length string)))
-          when pos do (write-string replacement out)
-          while pos)))
-
-(defun attrEndVal ()
-  (concatenate 'string ">"))
-
-(defun attributeValStr (attributeValues)
+(defun itemListStr (attributeValues)
 	(if (eql (cdr attributeValues) NIL)
 		(string (car attributeValues))
-		(concatenate 'string (string (car attributeValues)) 
-			";"
-			(paramstr (cdr attributeValues)))))
+			(concatenate 'string (string (car attributeValues))
+				";"
+			(itemListStr (cdr attributeValues)))))
 
-(defun attributesStr (attributes)
-	(concatenate 'string " " (string (car attributes)) "=\"" 
-		(attributeValStr (cdr attributes)) "\" "))
-		
-(defun contentStrVal (content)
-	(if (null content)
-		(concatenate 'string "" (string #\return) (string #\linefeed))
-		(concatenate 'string 
-			(car content)
-			(contentStrVal (cdr content)))))
-		
-(defun contentStrAttr (content)
-	(if (listp (car content))
-		(concatenate 'string 
-			(attributesStr (car content)) 
-			(contentStrAttr (cdr content)))
-		(if (null content)
-			(attrEndVal)
-			(concatenate 'string
-				(attrEndVal)
-				(contentStrVal content)))))
-		
+(defun propertiesStr (data)
+	(if (null data)
+		(values " />" nil)
+		(if (listp (car data))
+			(let ((singleProperty ""))
+				(setf singleProperty (concatenate 'string " " (string (caar data)) "=\"" (itemListStr (cdar data)) "\"" ))
+				(multiple-value-bind (returnStr returnLst)(propertiesStr (cdr data))
+					(values (concatenate 'string singleProperty " " returnStr) returnLst)))
+			(values ">" data))))
+												
 (defun contentStr (content)
-	(if (listp (car content))
-		(contentStrAttr content)
-		(concatenate 'string 
-			(attrEndVal)
-			(contentStrVal content))))
-		
-			
-(defun html (tag &rest content)
-	(let* ((NL (concatenate 'string (string #\return) (string #\linefeed)))
-		(NLTAB (concatenate 'string NL "    ")))
-		(replace-all
-			(if (null content)
-				(concatenate 'string
-					"<" (string tag) " />" )
-				(concatenate 'string
-					(string #\return) (string #\linefeed) "<" (string tag)
-					(contentStr content)
-					;(string #\return) (string #\linefeed)
-					"</" (string tag) ">" ))
-				NL NLTAB)))
-		
+	(if (null content)
+		""
+		(concatenate 'string (string (car content)) (contentStr (cdr content)))))
 
-;(trace attributeValStr attributesStr contentStrVal contentStrAttr contentStr html)
+(defun html (tag &rest data)
+	(concatenate 'string 
+		"<" (string tag)
+		(multiple-value-bind (strProp content) (propertiesStr data)
+			(concatenate 'string 
+				strProp
+				(if (null content)
+					""
+					(concatenate 'string 
+						(contentStr content)
+						"</ " (string tag) ">"))))))
+
+
+;(trace itemListStr propertiesStr contentStr html)
 
 
 (set-macro-character #\[
@@ -81,24 +46,24 @@ is replaced with replacement."
 
 (defun testhtml ()
 (princ (html :html
-			(html :head
-				(html :title "testtitel"))
-			(html :body
-				(html :h1 '(:id "erster Absatz") "It works!")))))
-				
-				
+(html :head
+(html :title "testtitel"))
+(html :body
+(html :h1 '(:id "erster Absatz") "It works!")))))
+
+
 (defun testhtml2 ()
 (html :html
-			(html :head
-				(html :title "testtitel"))
-			(html :body
-				(html :h1 '(:id "erster Absatz") "It works!"))))
-				
+(html :head
+(html :title "testtitel"))
+(html :body
+(html :h1 '(:id "erster Absatz") "It works!"))))
+
 (defun testhtml3 ()
-	[:html
-		[:head
-			[:title "testtitel" ]]
-		[:body
-			[:h1 '(:id "absatz" "abschnitt") "It works!" ]]])
-				
+[:html
+[:head
+[:title "testtitel" ]]
+[:body
+[:h1 '(:id "absatz" "abschnitt") "It works!" ]]])
+
 (testhtml)
